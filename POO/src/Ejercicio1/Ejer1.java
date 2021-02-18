3c471f12e394bb946be99fb97493a0f67bd2b9e7
aherr <aherr@DESKTOP-RP0NVQB>
2021-02-17 18:12:34 -0600

    x

---
+++ POO/src/Ejercicio1/Ejer1.java
@@ -0,0 +1,14 @@
+package Ejercicio1;
+
+public class Ejer1 {
+
+	public static void main(String[] args) {
+		
+		double a = 10, b = 15, c = 25;
+		double suma = a + b + c;
+		
+		System.out.println("La suma de las variables es: " + suma);
+
+	}
+
+}
